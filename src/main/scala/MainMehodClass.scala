b409551f91ca0f740362defdf836c1cd6ce74afc
naveenreddivari2019 <naveen.reddivari@gmail.com>
2021-12-28 17:53:13 +0100

    Initial Project setup

---
+++ src/main/scala/MainMehodClass.scala
@@ -0,0 +1,10 @@
+object MainMehodClass {
+
+  def main(args:Array[String])={
+    println("Main method running....")
+
+    val obj1=Student(1,"Naveen")
+    println(obj1.id+" | "+obj1.name)
+  }
+
+}
